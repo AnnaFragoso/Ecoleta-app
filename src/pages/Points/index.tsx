e62ed69dbbec2ca49a281980f9f91620050ff93c
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-06-07 19:31:58 -0300

    Busca de UF e cidade

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Constants from 'expo-constants';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather as Icon } from '@expo/vector-icons';
 import { View, Image, StyleSheet, TouchableOpacity, Text, ScrollView, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
@@ -22,6 +22,11 @@ interface Point {
     longitude: number;
 }
 
+interface Params {
+    uf: string;
+    city: string;
+}
+
 const Points = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
@@ -29,19 +34,22 @@ const Points = () => {
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
 
     useEffect(() => {
         api.get('points', {
             params: {
-                city: 'São Paulo',
-                uf: 'SP',
-                items: [1, 2]
+                city: routeParams.city,
+                uf: routeParams.uf,
+                items: selectedItems
             }
         }).then(response => {
             console.log(response.data)
             setPoints(response.data);
         })
-    }, []);
+    }, [selectedItems]);
 
     useEffect(() => {
         async function loadPosition() {
